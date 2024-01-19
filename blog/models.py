d5444fb1dbaa5f6b6c3ab37b7ba6bbac6485c34e
milan-c-00 <cvijovic.m21@gmail.com>
2024-01-19 13:33:20 +0100

    Added bootstrap and jobs are showing up

--- blog/models.py
+++ blog/models.py
@@ -1,2 +1,9 @@
 from django.db import models
 
+
+class Blog(models.Model):
+    title = models.CharField(max_length=250)
+    pub_date = models.DateTimeField()
+    body = models.TextField()
+    image = models.ImageField(upload_to='images/')
+
