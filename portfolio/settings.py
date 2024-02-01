eecfb5c7e35687cbca845e0b36cd90267996da62
milan-c-00 <cvijovic.m21@gmail.com>
2024-02-01 14:53:01 +0100

    Added local support

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -137,3 +137,10 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
+
